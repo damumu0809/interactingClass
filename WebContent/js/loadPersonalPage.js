5d8153930944778934d6e57b5a4ff51e8a423ebf
damumu0809 <1941096074@qq.com>
2016-07-19 16:48:36 +0800

    打印

--- WebContent/js/loadPersonalPage.js
+++ WebContent/js/loadPersonalPage.js
@@ -198,21 +198,21 @@
             });
 
 
-            for(slide; slide > 0; slide--){
-                $("#slider"+slide).responsiveSlides({
-                    auto: true,
-                    pager: true,
-                    nav: true,
-                    speed: 500,
-                    namespace: "callbacks",
-                    before: function () {
-                        $('.events').append("<li>before event fired.</li>");
-                    },
-                    after: function () {
-                        $('.events').append("<li>after event fired.</li>");
-                    }
-                });
-            }
+            //for(slide; slide > 0; slide--){
+            //    $("#slider"+slide).responsiveSlides({
+            //        auto: true,
+            //        pager: true,
+            //        nav: true,
+            //        speed: 500,
+            //        namespace: "callbacks",
+            //        before: function () {
+            //            $('.events').append("<li>before event fired.</li>");
+            //        },
+            //        after: function () {
+            //            $('.events').append("<li>after event fired.</li>");
+            //        }
+            //    });
+            //}
 
 
 
--- WebContent/js/loadStatisticPage.js
+++ WebContent/js/loadStatisticPage.js
@@ -1,4 +1,22 @@
-$.post("/interactingClass/Statistic", function (res) {
+
+    var spread = new GcSpread.Sheets.Spread(document.getElementById("ss"));
+
+    initSpread(spread);
+
+    $("#btnPrint").click(function () {
+        // used to adjust print range, should set with printInfo (refer custom print for detail)
+        //spread.sheets[0].setText(31, 11, " ");
+
+        spread.print();
+    });
+    function initSpread(spread) {
+        var sheet = spread.getActiveSheet();
+
+        sheet.isPaintSuspended(true);
+        sheet.allowCellOverflow(true);
+        sheet.setName("blog");
+
+        $.post("/interactingClass/Statistic", function (res) {
             var message = $.parseJSON(res);
             console.log(message);
 
@@ -9,45 +27,101 @@ $.post("/interactingClass/Statistic", function (res) {
             $("#userNum").text("总用户："+userNum);
             $("#blogNum").text("总博客数："+blogNum);
 
+
+            sheet.addSpan(1, 1, 1, 3);
+            sheet.setValue(1, 1, "总用户");
+            sheet.addSpan(1, 4, 1, 1);
+            sheet.setValue(1, 4, userNum );
+            sheet.addSpan(1, 5, 1, 3);
+            sheet.setValue(1, 5, "总博客");
+            sheet.addSpan(1, 8, 1, 1);
+            sheet.setValue(1, 8, blogNum);
+            sheet.addSpan(2, 1, 1, 4);
+            sheet.setValue(2, 1, "用户名");
+            sheet.addSpan(2, 5, 1, 1);
+            sheet.setValue(2, 5, "已发博客");
+            sheet.addSpan(2, 6, 1, 1);
+            sheet.setValue(2, 6, "赞");
+            sheet.addSpan(2, 7, 1, 1);
+            sheet.setValue(2, 7, "评论");
+            sheet.addSpan(2, 8, 1, 1);
+            sheet.setValue(2, 8, "占比");
+            sheet.addSpan(2, 9, 1, 4);
+            sheet.setValue(2, 9, "图示");
+
             userBlog.forEach(function(item, index, array){
                 var username = item.username;
                 var blog = item.blog;
                 var likeNum = item.likeNum;
                 var commentNum = item.commentNum;
 
-                var txt = '<tr>'+
-                    '<td>'+username+'</td>'+
-                    '<td>'+blog+'</td>'+
-                    '<td>'+likeNum+'</td>'+
-                    '<td>'+commentNum+'</td>'+
-                    '</tr>';
-                $("table").append(txt);
+                //var txt = '<tr>'+
+                //    '<td>'+username+'</td>'+
+                //    '<td>'+blog+'</td>'+
+                //    '<td>'+likeNum+'</td>'+
+                //    '<td>'+commentNum+'</td>'+
+                //    '</tr>';
+                //$("table").append(txt);
+                sheet.addSpan(index+3, 1, 1, 4);
+                sheet.setValue(index+3, 1, username);
+                sheet.addSpan(index+3, 5, 1, 1);
+                sheet.setValue(index+3, 5, blog);
+                sheet.addSpan(index+3, 6, 1, 1);
+                sheet.setValue(index+3, 6, likeNum);
+                sheet.addSpan(index+3, 7, 1, 1);
+                sheet.setValue(index+3, 7, commentNum);
+
+                var t  = blog/blogNum;
+
+                sheet.addSpan(index+3, 8, 1, 1);
+                sheet.setValue(index+3, 8, t);
+
+                //var colors = ["#0099FF", "#33FFFF", "#9E0142", "#D53E4F", "#F46D43", "#FDAE61", "#FEE08B", "#E6F598", "#ABDDA4", "#66C2A5", "#3288BD", "#5E4FA2"];
+                sheet.addSpan(index+3, 9, 1, 4);
+
+                sheet.setFormula(index+3, 9, '=HBARSPARKLINE('+t+',"#D53E4F")');
             });
 
-});
+
+            sheet.getCells(1, 1, 1, 3).backColor("#D9D9FF");
+            sheet.getCells(1, 5, 1, 7).backColor("#D9D9FF");
+            sheet.getCells(2, 1, 2, 8).backColor("#D9FFD9");//两点坐标
+           // sheet.text().hAlign(GcSpread.Sheets.HorizontalAlign.center);
 
 
-    function AutomateExcel()
-    {
+            //sheet.addSpan(31, 5, 1, 4);
+            //sheet.getCell(31, 5).text("Figure 1").hAlign(1);
+        });
 
-        var oXL = new ActiveXObject("Excel.Application"); //创建应该对象
-        var oWB = oXL.Workbooks.Add();//新建一个Excel工作簿
-        var oSheet = oWB.ActiveSheet;//指定要写入内容的工作表为活动工作表
-        var table = document.all.data;//指定要写入的数据源的id
-        var hang = table.rows.length;//取数据源行数
-        var lie = table.rows(0).cells.length;//取数据源列数
 
-// Add table headers going cell by cell.
-        for (i=0;i<hang;i++){//在Excel中写行
-            for (j=0;j<lie;j++){//在Excel中写列
-//定义格式
-                oSheet.Cells(i+1,j+1).NumberFormatLocal = "@";
-//!!!!!!!上面这一句是将单元格的格式定义为文本
-                oSheet.Cells(i+1,j+1).Font.Bold = true;//加粗
-                oSheet.Cells(i+1,j+1).Font.Size = 10;//字体大小
-                oSheet.Cells(i+1,j+1).value = table.rows(i).cells(j).innerText;//向单元格写入值
-            }
-        }
-        oXL.Visible = true;
-        oXL.UserControl = true;
+
+        //sheet.setBorder(new GcSpread.Sheets.Range(1, 1, 18, 10), new GcSpread.Sheets.LineBorder("Black", GcSpread.Sheets.LineStyle.thin), { all: true });
+        //sheet.setBorder(new GcSpread.Sheets.Range(4, 4, 3, 6), new GcSpread.Sheets.LineBorder("Black", GcSpread.Sheets.LineStyle.dotted), { inside: true });
+        //sheet.setBorder(new GcSpread.Sheets.Range(7, 4, 3, 6), new GcSpread.Sheets.LineBorder("Black", GcSpread.Sheets.LineStyle.dotted), { inside: true });
+        //sheet.setBorder(new GcSpread.Sheets.Range(11, 4, 3, 6), new GcSpread.Sheets.LineBorder("Black", GcSpread.Sheets.LineStyle.dotted), { inside: true });
+        //sheet.setBorder(new GcSpread.Sheets.Range(14, 4, 3, 6), new GcSpread.Sheets.LineBorder("Black", GcSpread.Sheets.LineStyle.dotted), { inside: true });
+
+        //fillSampleData(sheet, new GcSpread.Sheets.Range(4, 4, 6, 6));
+        //fillSampleData(sheet, new GcSpread.Sheets.Range(11, 4, 6, 6));
+
+        //sheet.setColumnWidth(0, 40);
+        //sheet.setColumnWidth(1, 40);
+        //sheet.setColumnWidth(3, 40);
+        //sheet.setColumnWidth(4, 40);
+        //sheet.setColumnWidth(11, 40);
+
+        sheet.isPaintSuspended(false);
     }
+
+    //function fillSampleData(sheet, range) {
+    //    for (var i = 0; i < range.rowCount; i++) {
+    //        for (var j = 0; j < range.colCount; j++) {
+    //            sheet.setValue(range.row + i, range.col + j, Math.ceil(Math.random() * 300));
+    //        }
+    //    }
+    //}
+
+
+
+
+
